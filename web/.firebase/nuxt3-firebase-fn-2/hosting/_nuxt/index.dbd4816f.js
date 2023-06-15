3f195f7d25d7514d86ee371f142699d40cb701ac
Charles Chen <chen.charles.c@gmail.com>
2023-06-15 19:33:21 -0400

    Removed .firebase and added exclusion

--- web/.firebase/nuxt3-firebase-fn-2/hosting/_nuxt/index.dbd4816f.js
+++
@@ -1,1 +0,0 @@
-import{a as e,b as t,o as c}from"./entry.e17c78b9.js";const n={};function a(o,r){return c(),t("div",null," This is the default page. ")}const _=e(n,[["render",a]]);export{_ as default};
